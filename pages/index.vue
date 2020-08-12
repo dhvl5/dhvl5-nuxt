58be4985bf45b39a83368677b25e83d2c94d5ae3
Dhaval <prajapatidhaval304@gmail.com>
2020-08-13 01:24:55 +0530

    contact form

--- pages/index.vue
+++ pages/index.vue
@@ -50,7 +50,7 @@
           <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg> -->
-          <span class="ml-3 text-xl text-text-text-color">DHVL5</span>
+          <span class="ml-3 text-xl text-white">DHVL5</span>
         </a>
         <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#aboutme" class="mr-5 font-bold transform ease-in-out duration-200 hover:text-green-500">About Me</a>
@@ -76,43 +76,43 @@
     </header>
     <main class="h-full w-full divide-y-2 divide-white divide-opacity-25">
       <div class="mt-0 sm:mt-10 md:mt-10 lg:mt-10 xl:mt-32 w-full flex px-5 py-24 items-center flex-col">
-        <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center text-text-text-color">
+        <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center text-white">
           <h3 class="mb-8">HOWDY-DOODY!</h3>
-          <h1 class="title-font sm:text-7xl text-5xl mb-8 font-medium text-text-text-color leading-none">I am
+          <h1 class="title-font sm:text-7xl text-5xl mb-8 font-medium text-white leading-none">I am
             <br class="hidden lg:inline-block"><p class="text-green-700">Dhaval Prajapati.</p>
           </h1>
           <p class="mb-8 text-xl leading-relaxed">a passionate self-made game developer <br>who loves coding and making cool stuff with it.</p>
           <div class="flex justify-center">
-            <a href="#contactme" class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Let's grab a coffee</a>
+            <a href="#contactme" class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Say 'hi'</a>
           </div>
         </div>
         <div class="pt-0 xl:pt-24 w-full h-64 flex justify-center items-center">
-          <a href="#aboutme"><svg class="animate-bounce mt-4 ml-4 w-10 h-10 text-text-text-color" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+          <a href="#aboutme"><svg class="animate-bounce mt-4 ml-4 w-10 h-10 text-white" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
             <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
           </svg></a>
         </div>
       </div>
       <div id="aboutme" class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
         <!-- <p class="text-white text-2xl pb-16">About Me</p> -->
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 pb-16 text-text-text-color flex justify-center">About Me</h1>
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 pb-16 text-white flex justify-center">About Me</h1>
         <img class="lg:w-3/12 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded-md" alt="hero" src="me_main.jpg">
         <div class="text-center lg:w-2/3 w-full">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-text-text-color">Hello it's me again!</h1>
-          <p class="leading-relaxed mb-8 text-text-text-color">I've always loved games more than anything, I have so many ideas with so much potential. I want to create this and that and so on, but then I started working as a Data Entry Clerk while pursuing my studies further, those were some of the worst days ever. The job was killing me from inside before I decided to drop out of college to do what I actually wanted to do. Since then, I've completed an internship and taught myself Unity3D within a few months, I am currently working within the industry and am actively creating 2D, 3D and AR apps/games.</p>
+          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Hello it's me again!</h1>
+          <p class="leading-relaxed mb-8 text-white">I've always loved games more than anything, I have so many ideas with so much potential. I want to create this and that and so on, but then I started working as a Data Entry Clerk while pursuing my studies further, those were some of the worst days ever. The job was killing me from inside before I decided to drop out of college to do what I actually wanted to do. Since then, I've completed an internship and taught myself Unity3D within a few months, I am currently working within the industry and am actively creating 2D, 3D and AR apps/games.</p>
         </div>
       </div>
       <div id="experience" class="container px-5 py-24 mx-auto">
         <!-- <p class="text-white text-2xl pb-16 flex justify-center">Experience</p> -->
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 pb-16 text-text-text-color flex justify-center">Experience</h1>
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 pb-16 text-white flex justify-center">Experience</h1>
         <div class="my-8">
           <div class="py-8 flex flex-wrap md:flex-no-wrap">
             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <span class="tracking-widest font-medium title-font text-text-text-color">Unity Developer</span>
+              <span class="tracking-widest font-medium title-font text-white">Unity Developer</span>
               <span class="mt-1 text-gray-600 text-sm">JUNE 2020 - PRESENT</span>
             </div>
             <div class="md:flex-grow">
-              <h2 class="text-2xl font-medium text-text-text-color title-font mb-2">The Intellify</h2>
-              <p class="leading-relaxed text-text-text-color">- Creating virtual and augmented reality applications<br>- Building complex logic and problem solving</p>
+              <h2 class="text-2xl font-medium text-white title-font mb-2">The Intellify</h2>
+              <p class="leading-relaxed text-white">- Creating virtual and augmented reality applications<br>- Building complex logic and problem solving</p>
               <a href="https://theintellify.com/" target="_blank" class="text-green-500 inline-flex items-center mt-4">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
@@ -123,12 +123,12 @@
           </div>
           <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-no-wrap">
             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <span class="tracking-widest font-medium title-font text-text-text-color">Unity Developer</span>
+              <span class="tracking-widest font-medium title-font text-white">Unity Developer</span>
               <span class="mt-1 text-gray-600 text-sm">SEPTEMBER 2018 - APRIL 2020</span>
             </div>
             <div class="md:flex-grow">
-              <h2 class="text-2xl font-medium text-text-text-color title-font mb-2">CreArt Solutions</h2>
-              <p class="leading-relaxed text-text-text-color">- Creating games and augmented reality applications<br>- Building complex logic and problem solving<br>- Teaching game development</p>
+              <h2 class="text-2xl font-medium text-white title-font mb-2">CreArt Solutions</h2>
+              <p class="leading-relaxed text-white">- Creating games and augmented reality applications<br>- Building complex logic and problem solving<br>- Teaching game development</p>
               <a href="https://creart.in/" target="_blank" class="text-green-500 inline-flex items-center mt-4">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
@@ -139,12 +139,12 @@
           </div>
           <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-no-wrap">
             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <span class="tracking-widest font-medium title-font text-text-text-color">Data Entry Clerk</span>
+              <span class="tracking-widest font-medium title-font text-white">Data Entry Clerk</span>
               <span class="mt-1 text-gray-600 text-sm">MAY 2018 - SEPTEMBER 2018</span>
             </div>
             <div class="md:flex-grow">
-              <h2 class="text-2xl font-medium text-text-text-color title-font mb-2">Anand Tradelink Pvt. Ltd.</h2>
-              <p class="leading-relaxed text-text-text-color"> - Created bills, quotations based on orders and continuous communication with clients.</p>
+              <h2 class="text-2xl font-medium text-white title-font mb-2">Anand Tradelink Pvt. Ltd.</h2>
+              <p class="leading-relaxed text-white"> - Created bills, quotations based on orders and continuous communication with clients.</p>
               <a href="https://atpl.biz/" target="_blank" class="text-green-500 inline-flex items-center mt-4">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
@@ -157,22 +157,36 @@
       </div>
       <div id="contactme" class="container px-5 pt-24 xl:py-24 mx-auto">
         <div class="flex flex-col text-center w-full mb-12">
-          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-text-text-color">Contact Me</h1>
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Contact Me</h1>
           <!-- <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p> -->
         </div>
         <div class="lg:w-1/2 md:w-2/3 mx-auto">
           <div class="flex flex-wrap -m-2">
             <div class="p-2 w-1/2">
-              <input class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-green-500 text-base px-4 py-2" placeholder="Name" type="text">
+              <input id="nameInput" v-model="form.name" class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-green-500 text-base px-4 py-2" placeholder="Name" type="text" name="name" required>
             </div>
             <div class="p-2 w-1/2">
-              <input class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-green-500 text-base px-4 py-2" placeholder="Email" type="email">
+              <input id="emailInput" v-model="form.email" class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-green-500 text-base px-4 py-2" placeholder="Email" type="email" name="email" required>
             </div>
             <div class="p-2 w-full">
-              <textarea class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none h-48 focus:border-green-500 text-base px-4 py-2 resize-none block" placeholder="Message"></textarea>
+              <textarea id="msgInput" v-model="form.msg" class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none h-48 focus:border-green-500 text-base px-4 py-2 resize-none block" placeholder="Message" name="message" required></textarea>
+            </div>
+            <div id="errorAlert" class="bg-red-100 hidden border border-red-400 text-red-700 p-2 w-full rounded relative" role="alert">
+              <strong class="font-bold">Nah!</strong>
+              <span class="sm:inline">All fields are required.</span>
+              <span class="absolute top-0 bottom-0 right-0 px-4 py-2">
+                <svg @click="hideError" class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+              </span>
+            </div>
+            <div id="successAlert" class="bg-green-100 hidden border border-green-400 text-green-700 p-2 w-full rounded relative" role="alert">
+              <strong class="font-bold">Thanks!</strong>
+              <span class="sm:inline">I'll get back to you ASAP.</span>
+              <span class="absolute top-0 bottom-0 right-0 px-4 py-2">
+                <svg @click="hideSuccess" class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+              </span>
             </div>
             <div class="mt-12 p-2 w-full">
-              <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Submit</button>
+              <button @click="submitForm" class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Submit</button>
             </div>
             <div class="p-2 w-full pt-8 mt-8 border-t border-gray-800 text-center">
               <!-- <a class="text-green-500">prajapatidhaval304@gmail.com</a><br>
@@ -218,7 +232,7 @@
         </div>
       </dialog>
     </main>
-    <footer class="text-gray-500 bg-background-primary body-font">
+    <footer class="text-gray-500 bg-gray-900 body-font">
       <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <!-- <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
@@ -243,7 +257,12 @@ const theme = document.body
 export default {
   data() {
     return {
-      toggle: false
+      toggle: false,
+      form: {
+        name: "",
+        email: "",
+        msg: ""
+      }
     }
   },
   created() {
@@ -263,6 +282,38 @@ export default {
       this.toggle = !this.toggle
       theme.classList.toggle('theme-dark', this.toggle)
       localStorage.setItem('toggle', this.toggle)
+    },
+    submitForm() {
+      if(document.getElementById('nameInput').value == '') { this.showError(); return; }
+      if(document.getElementById('emailInput').value == '') { this.showError(); return; }
+      if(document.getElementById('msgInput').value == '') { this.showError(); return; }
+
+      let formData = new FormData();
+      formData.append("name", this.form.name);
+      formData.append("email", this.form.email);
+      formData.append("message", this.form.msg);
+
+      fetch("https://formsubmit.co/ajax/prajapatidhaval304@gmail.com", { body: formData, method: "post" })
+      .then(response => response.status)
+      .then(data => { if(data == 200) { this.showSuccess(); } });
+
+      
+
+      document.getElementById('nameInput').value = '';
+      document.getElementById('emailInput').value = '';
+      document.getElementById('msgInput').value = '';
+    },
+    hideError() {
+      document.getElementById('errorAlert').style.display = "none";
+    },
+    showError() {
+      document.getElementById('errorAlert').style.display = "block";
+    },
+    hideSuccess() {
+      document.getElementById('successAlert').style.display = "none";
+    },
+    showSuccess() {
+      document.getElementById('successAlert').style.display = "block";
     }
   }
 }
