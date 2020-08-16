9ece3c467aed082bba9191bf9902d33aa251d474
Dhaval <prajapatidhaval304@gmail.com>
2020-08-17 00:18:34 +0530

    email validation

--- pages/index.vue
+++ pages/index.vue
@@ -115,7 +115,7 @@
               <input id="nameInput" v-model="form.name" class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-green-500 text-base px-4 py-2" placeholder="Name" type="text" name="name" required>
             </div>
             <div class="p-2 w-1/2">
-              <input id="emailInput" v-model="form.email" class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-green-500 text-base px-4 py-2" placeholder="Email" type="email" name="email" required>
+              <input id="emailInput" v-model="form.email" class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-green-500 text-base px-4 py-2" placeholder="Email" type="email" name="email" pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" required>
             </div>
             <div class="p-2 w-full">
               <textarea id="msgInput" v-model="form.msg" class="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none h-48 focus:border-green-500 text-base px-4 py-2 resize-none block" placeholder="Message" name="message" required></textarea>
@@ -125,6 +125,13 @@
               <span class="sm:inline">All fields are required.</span>
               <span class="absolute top-0 bottom-0 right-0 px-4 py-2">
                 <svg @click="hideError" class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+              </span>
+            </div>
+            <div id="emailAlert" class="bg-red-100 hidden border border-red-400 text-red-700 p-2 w-full rounded relative" role="alert">
+              <strong class="font-bold">Oops!</strong>
+              <span class="sm:inline">It seems that's not a correct mail id.</span>
+              <span class="absolute top-0 bottom-0 right-0 px-4 py-2">
+                <svg @click="hideEmail" class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
               </span>
             </div>
             <div id="successAlert" class="bg-green-100 hidden border border-green-400 text-green-700 p-2 w-full rounded relative" role="alert">
@@ -210,6 +217,7 @@
 
 <script>
 const theme = document.body
+var mailMatch = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 export default {
   data() {
@@ -235,6 +243,8 @@ export default {
       if(document.getElementById('nameInput').value == '') { this.showError(); return; }
       if(document.getElementById('emailInput').value == '') { this.showError(); return; }
       if(document.getElementById('msgInput').value == '') { this.showError(); return; }
+      if(!document.getElementById('emailInput').value.match(mailMatch))
+      { this.showEmailError(); return; }
 
       let formData = new FormData();
       formData.append("name", this.form.name);
@@ -248,12 +258,20 @@ export default {
       document.getElementById('nameInput').value = '';
       document.getElementById('emailInput').value = '';
       document.getElementById('msgInput').value = '';
+      document.getElementById('errorAlert').style.display = "none";
+      document.getElementById('emailAlert').style.display = "none";
     },
     hideError() {
       document.getElementById('errorAlert').style.display = "none";
     },
     showError() {
       document.getElementById('errorAlert').style.display = "block";
+    },
+    showEmailError() {
+      document.getElementById('emailAlert').style.display = "block";
+    },
+    hideEmail() {
+      document.getElementById('emailAlert').style.display = "none";
     },
     hideSuccess() {
       document.getElementById('successAlert').style.display = "none";
